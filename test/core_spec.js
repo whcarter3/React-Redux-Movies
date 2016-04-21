657de7c567334c44a043e4ee600316274b81bc73
Will Carter <whcarter3@gmail.com>
2016-04-20 20:21:55 -0700

    modularized core functions not to pass whole state

--- test/core_spec.js
+++ test/core_spec.js
@@ -39,44 +39,33 @@ describe('application logic', () =>{
   describe('vote', () => {
     it('creates a tally for the voted entry', () => {
       const state = Map({
-        vote: Map({
-	  pair: List.of('Trainspotting', '28 Days Later')
-	}),
-	entries: List()
-      });
+	     pair: List.of('Trainspotting', '28 Days Later')
+	    });
       const nextState = vote(state, 'Trainspotting');
       expect(nextState).to.equal(Map({
-        vote: Map({
-	  pair: List.of('Trainspotting', '28 Days Later'),
-	  tally: Map({
-	    'Trainspotting': 1
-	  })
-	}),
-	  entries: List()
-      }));
+            pair: List.of('Trainspotting', '28 Days Later'),
+            tally: Map({
+                    'Trainspotting': 1
+                })
+        }));
     });
 
     it('adds to existing tally for the voted entry', () =>{
       const state = Map({
-	vote: Map({
-	  pair: List.of('Trainspotting', '28 Days Later'),
-	  tally: Map({
-	    'Trainspotting': 3,
-	    '28 Days Later': 2
-	  })
-	}),
-	entries: List()
+	    pair: List.of('Trainspotting', '28 Days Later'),
+	    tally: Map({
+	        'Trainspotting': 3,
+	        '28 Days Later': 2
+	    })
       });
-      const nextState= Map({
-	vote: Map({
-	  pair: List.of('Trainspotting', '28 Days Later'),
-	  tally: Map({
-	    'Trainspotting': 4,
-	    '28 Days Later': 2
-	  })
-	}),
-	entries: List()
-      });
+      const nextState = vote(state, 'Trainspotting');
+      expect(nextState).to.equal(Map({
+            pair: List.of('Trainspotting', '28 Days Later'),
+            tally: Map({
+                    'Trainspotting': 4,
+                    '28 Days Later': 2
+                })
+          }));
     });
 
     it('puts winner of current vote back to entries', () => {
